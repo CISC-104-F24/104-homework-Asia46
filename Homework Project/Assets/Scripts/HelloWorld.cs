e8a4f84701a24cb9afb7e0c0ee106d0f8b11d61f
AsiaSantiago <asialeen.santiago12@gmail.com>
2024-09-05 23:50:06 -0400

    FInal Submission Homework 0

--- Homework Project/Assets/Scripts/HelloWorld.cs
+++ Homework Project/Assets/Scripts/HelloWorld.cs
@@ -7,7 +7,15 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      Debug.Log("Hello, world!");  
+      Debug.Log("Hello, world!"); 
+      Debug.LogWarning("Hello, I am a warning!");
+      Debug.LogError("ERROR! ERROR!");
+      // Author: Asia 
+      /* A game devlopment idea I have is a isometic cat cafe mobile game. 
+       *  I don't have much figured out for it but I want it to be a two story cafe layout
+       * where the main controls is tapping. 
+       */
+      
     }
 
     // Update is called once per frame
