58d943dc86829c3b5ed3059c59b9b674606207c6
AsiaSantiago <asialeen.santiago12@gmail.com>
2024-11-19 11:45:29 -0500

    complete

--- Homework Project/Assets/Scripts/Card.cs
+++ Homework Project/Assets/Scripts/Card.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Card : MonoBehaviour
 {
+    public int health; 
+    public int power; 
+    public string name; 
+    public TextMeshProUGUI rightShield; 
+    public TextMeshProUGUI LeftAttack; 
+    public Vector3 Tapped= new Vector3(0,0,-90); 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +24,31 @@ public class Card : MonoBehaviour
     {
         
     }
+
+    public void TakeDamage(int damage)
+    {
+        health=health-damage;
+        rightShield.text=health.ToString();
+    }
+
+    public void AddCounter(int counter)
+    { 
+        power=power-counter; 
+        LeftAttack.text=power.ToString(); 
+      
+       if (power>5)
+       { 
+        LeftAttack.color= new Color(0,1,1,1);
+       }
+       
+    }
+     
+    public void IsTapped(bool turned )
+    {
+        if (turned)
+        { 
+           transform.eulerAngles=Tapped;
+        }
+    }
+
 }
